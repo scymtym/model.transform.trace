6a332ac533c41c4904e944e6d63eb3d777bee75c
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-10-28 15:33:33 +0200

    Updated example in examples/simple-transform.lisp

--- examples/simple-transform.lisp
+++ examples/simple-transform.lisp
@@ -8,12 +8,18 @@
 (cl:in-package #:model.transform.trace.examples.simple-transform)
 
 (defun tracing-1+ (x)
-  (ensured-transform* ('tracing-1+ x)
+  (ensured-transform (((make-symbol "TRACING-1+")) x)
     (1+ x)))
 
 (defun tracing-+ (x y)
-  (ensured-transform* ('tracing-+ x y)
+  (ensured-transform (((make-symbol "TRACING-+")) x y)
     (+ x y)))
+
+(defun computation ()
+  (let* ((intermediate (tracing-1+ (tracing-+ 1 2)))
+         (result       (tracing-+ (tracing-1+ (tracing-1+ intermediate))
+                                  (tracing-+ intermediate intermediate))))
+    result))
 
 (with-tracer ((make-instance 'tracer))
   (let* ((intermediate (tracing-1+ (tracing-+ 1 2)))
@@ -21,8 +27,8 @@
                                   (tracing-+ intermediate intermediate))))
 
     ;;
-    (labels ((visit (recurse source target &optional (depth 1))
-               (format t "~V<~>~S <- ~S~%" (* 2 depth) target source)
+    (labels ((visit (recurse transform source target &optional (depth 1))
+               (format t "~V<~>~S <-~A- ~S~%" (* 2 depth) target transform source)
                (funcall recurse :function (rcurry #'visit (1+ depth)))))
 
       (format t "~2&~S~%" result)
